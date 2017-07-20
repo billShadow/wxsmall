878b2c84ceb43fd400e649f6bf647dc5e67187ec
xiaobin <xiaobin@e7124.com>
2017-07-20 16:31:33 +0800

    test

--- pages/mycenter/mycenter.js
+++ pages/mycenter/mycenter.js
@@ -1,4 +1,3 @@
-//index.js
 //获取应用实例
 var app = getApp()
 Page({
@@ -7,19 +6,19 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../mycenter/mycenter'
     })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
