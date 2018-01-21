cb646f91f80b052421f23eaf50c265ad63605222
hock <2392985460@qq.com>
2018-01-22 00:15:00 +0800

    home page insert 2

--- pages/home/home.js
+++ pages/home/home.js
@@ -9,16 +9,16 @@ Page({
     ],
     lists: [
       {
-        url: '../../images/fy1-img.jpg', tit: '整租 · 南竹杆胡同1居室-南' },
-      { url: '../../images/fy2-img.jpg', tit: '整租 · 海特花园小区2居室-南北' },
-      { url: '../../images/fy3-img.jpg', tit: '整租 · 未来661居室-南' },
-      { url: '../../images/fy1-img.jpg', tit: '整租 · 南竹杆胡同1居室-南' }
+        url: 'http://ocs2ic4ow.bkt.clouddn.com/fy1-img.jpg', tit: '整租 · 南竹杆胡同1居室-南' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/fy2-img.jpg', tit: '整租 · 海特花园小区2居室-南北' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/fy3-img.jpg', tit: '整租 · 未来661居室-南' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/fy1-img.jpg', tit: '整租 · 南竹杆胡同1居室-南' }
     ],
     lgImg: [
-      { url: '../../images/lg1.png' },
-      { url: '../../images/lg1.png' },
-      { url: '../../images/lg1.png' },
-      { url: '../../images/lg1.png' }
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/lg1.png' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/lg1.png' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/lg1.png' },
+      { url: 'http://ocs2ic4ow.bkt.clouddn.com/lg1.png' }
     ],
     array: [{
       mode: 'scaleToFill',
